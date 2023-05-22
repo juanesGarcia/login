c79cb609f7f715468859e95f72911f86ce76d009
juanes <67403119+juanesGarcia@users.noreply.github.com>
2023-05-22 15:32:10 -0500

    f

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/login-facebook', function () {
+    return Socialite::driver('facebook')->redirect();
+});
+Route::get('/facebook-callback', function () {
+    $user=Socialite::driver('facebook')->user();
+    dd($user);
+});
+Route::get('/login-google', function () {
+    return Socialite::driver('google')->redirect();
+});
+Route::get('/google-callback', function () {
+    $user = Socialite::driver('google')->user();
+    dd($user);
 });
