891970f44a1321ee2833817fa1bcf39a4421b386
juanes <67403119+juanesGarcia@users.noreply.github.com>
2023-05-22 22:58:58 -0500

    probando sin comillas

--- config/services.php
+++ config/services.php
@@ -31,14 +31,14 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'google' => [    
-        'client_id' => '28380013924-bfde9glto0ksg6cjcme5uaidrc8dji0c.apps.googleusercontent.com',  
-        'client_secret' => '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',  
-        'redirect' => '/google-callback', 
+        'client_id' => 28380013924-bfde9glto0ksg6cjcme5uaidrc8dji0c.apps.googleusercontent.com,  
+        'client_secret' => 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,  
+        'redirect' => "/google-callback", 
     ],
     'facebook' => [    
-        'client_id' => '3448057475459141',  
-        'client_secret' =>'cd5074a74891ea44fd30059775e55613',  
-        'redirect' => 'https://pruebadevops2.azurewebsites.net/facebook-callback',
+        'client_id' => 3448057475459141,  
+        'client_secret' =>cd5074a74891ea44fd30059775e55613,  
+        'redirect' => "https://pruebadevops2.azurewebsites.net/facebook-callback",
     ],
 
 ];
